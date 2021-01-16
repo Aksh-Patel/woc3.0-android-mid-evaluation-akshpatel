1126517cae75349f6d8d1f2db36874b5b543caf2
Aksh Patel <201901005@daiict.ac.in>
2021-01-16 21:36:35 +0530

    Init commit

---
+++ app/src/main/java/com/example/timetableapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.timetableapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void monday(View view) {
+        Intent intent = new Intent(this,monday.class);
+        startActivity(intent);
+    }
+    public void tuesday(View view) {
+        Intent intent = new Intent(this,tuesday.class);
+        startActivity(intent);
+    }
+    public void wednesday(View view) {
+        Intent intent = new Intent(this,wednesday.class);
+        startActivity(intent);
+    }
+    public void thursday(View view) {
+        Intent intent = new Intent(this,thursday.class);
+        startActivity(intent);
+    }
+    public void friday(View view) {
+        Intent intent = new Intent(this,friday.class);
+        startActivity(intent);
+    }
+    public void saturday(View view) {
+        Intent intent = new Intent(this,saturday.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
